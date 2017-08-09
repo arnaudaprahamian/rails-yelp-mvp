1a9624bb3f49c2c9c05f259f8620362ed02eeee8
arnaudaprahamian <arnaud.aprahamian@gmail.com>
2017-08-09 11:14:36 +0200

    initialize controller

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,7 @@
 class Restaurant < ApplicationRecord
   CATEGORIES = %w(chinese italian japanese french belgian)
   validates :name, :address, :category, presence: true
+  validates :name, uniqueness: true
   validates :category, inclusion: { in: CATEGORIES }
   has_many :reviews, dependent: :destroy
 end
